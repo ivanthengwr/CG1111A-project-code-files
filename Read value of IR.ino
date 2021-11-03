8bd0ad37ab401e5088448f160c57d39939f03dd7
Ivan Theng <88478793+ivanthengwr@users.noreply.github.com>
2021-11-03 21:23:03 +0800

    This file reads the raw value of IR in voltage

---
+++ Read value of IR.ino
@@ -0,0 +1,22 @@
+int A = A2;
+int B = A1;
+
+int readVal;
+int readPin = A0;
+float V2 = 0;
+void setup() {
+  pinMode(readPin, INPUT);
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(A,LOW);
+  digitalWrite(B, LOW);
+readVal = analogRead(readPin);
+V2 = (5./1023.)*readVal;
+Serial.println(V2);
+delay(100);
+}
